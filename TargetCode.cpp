092299fd968e42d2547da30d16adb11cf966aa5c
tomstream <tomstream@qq.com>
2016-06-23 00:07:27 +0800

    handle conflict

--- TargetCode.cpp
+++ TargetCode.cpp
@@ -1,3 +1,4 @@
+
 #include "TargetCode.h"
 #include "Parser.h"
 #include <vector>
@@ -361,4 +362,281 @@ void GenTargetCode(const vector<midcode>&){
 			// pop sth to end a block
 		}
 	}
+=======
+#include "TargetCode.h"
+#include "Parser.h"
+#include <vector>
+#include <string>
+#include <sstream>
+#include "symbol.h"
+#include <assert.h>
+Section dataSection;
+Section codeSection;
+
+string spaceStr[4] = { "BYTE" ,"SDWORD", "REAL4", "DWORD"};
+
+Space typeSpace(int simpleType)
+{
+	switch (simpleType) {
+		case SYS_TYPE_INTEGER:return Space::SDWORD;
+		case SYS_TYPE_REAL:return Space::REAL4;
+		case SYS_TYPE_CHAR:return Space::BYTE;
+		case SYS_TYPE_BOOL:return Space::BYTE;
+	}
+}
+
+void declare(string name, nodeType *node)
+{
+	Space space;
+	string valuestr = "?";
+	if (node->type == typeType) {
+		space = typeSpace(node->tp.type);
+	}
+	else if (node->type == typeId) {
+		//return node->id.sValue;
+	}
+	else if (node->type == typeOpr) {
+		if (node->opr.oper == ENUM) {
+			//return "<enumeration type>";
+			space = Space::DWORD;
+		}
+		else if (node->opr.oper == CONST_POSITIVE_POSITIVE || node->opr.oper == CONST_NEGATIVE_NEGATIVE || node->opr.oper == CONST_NEGATIVE_POSITIVE) {
+			//return "<subrange type>";
+			space = Space::SDWORD;
+		}
+		else if (node->opr.oper == ARRAY_TYPE_DECL) {
+			//return "array [" + type_str(node->opr.op[0]) + "] of " + type_str(node->opr.op[1]);
+			nodeType *index_node = node->opr.op[0];
+			int size = array_size(index_node);
+			//support simple type temporarily
+			assert(node->opr.op[1]->type == typeType);
+			space = typeSpace(node->opr.op[1]->tp.type);
+			valuestr = intToString(size);
+			valuestr += " dup(?)";
+		}
+		else if (node->opr.oper == RECORD_TYPE_DECL) {
+			vector<string> fields;
+			vector<nodeType*> types;
+			get_from_field_decl_list(fields, types, node->opr.op[0]);
+			for (int i = 0; i < fields.size();i++) 
+			{
+				string varName = name + "." + fields[i];
+				//support simple type temporarily
+				assert(types[i]->type == typeType);
+				declare(varName, types[i]);
+			}
+		}
+		else if (node->opr.oper == FUNCTION_HEAD) {
+			//return "<function>";
+		}
+		else if (node->opr.oper == PROCEDURE_HEAD) {
+			//return "<procedure>";
+		}
+	}
+	else if (node->type == typeCon) {
+		stringstream ss;
+		switch (node->con.type) {
+		case INTEGER: {
+			space = Space::SDWORD;
+			ss << node->con.integer;
+			ss >> valuestr;
+			break;
+		}
+		case STRING: {
+			space = Space::BYTE;
+			valuestr = node->con.str;
+			break;
+		}
+		case CHAR: {
+			space = Space::BYTE;
+			ss << node->con.character;
+			ss >> valuestr;
+			valuestr = "\'" + valuestr + "\'";
+			break;
+		}
+		case REAL: {
+			space = Space::REAL4;
+			ss << node->con.real;
+			ss >> valuestr;
+			break;
+		}
+		case BOOL: {
+			space = Space::BYTE;
+			ss << node->con.boolean;
+			ss >> valuestr;
+			break;
+		}
+		}
+	}
+	else if (node->type == typeSysProc) {
+		//return "<system procedure>";
+	}
+	else if (node->type == typeSysFunc) {
+		//return "<system function>";
+	}
+
+	string spacestr = spaceStr[space];
+	dataSection.append(name, spacestr, valuestr);
+}
+
+void printTargetCode(ostream & os)
+{
+	os << ".data" << endl;
+	os << dataSection;
+}
+
+void insertIOFormatlist(){
+	dataSection.append("zero", "BYTE", "0");
+	dataSection.append("intFmt", "BYTE", "\"%d\", 0");
+	dataSection.append("charFmt", "BYTE", "\"%c\", 0");
+	dataSection.append("realFmt", "BYTE", "\"%lf\", 0");
+	dataSection.append("stringFmt", "BYTE", "\"%s\", 0");
+	dataSection.append("TrueSentence", "BYTE", "\"true\", 0");
+	dataSection.append("FalseSentence", "BYTE", "\"false\", 0");
+}
+
+#include <utility>
+
+void writeArg(const string& arg, int type) {
+	//invoke crt_printf, addr fmtlist, area, area2
+	switch (type) {
+		case INTEGER: {
+			codeSection.append("invoke","crt_printf,addr intFmt,"+arg);
+		}break;
+		case REAL: {
+			codeSection.append("invoke", "crt_printf,addr realFmt," + arg);
+		}break;
+		case CHAR: {
+			codeSection.append("invoke", "crt_printf,addr charFmt," + arg);
+		}break;
+		case STRING: {
+			codeSection.append("invoke", "crt_printf,addr stringFmt,addr" + arg);
+		}break;
+		case BOOL: {
+			codeSection.append("invoke", "crt_printf,addr intFmt," + arg);
+		}break;
+	}
+}
+
+void sysProcWrite(vector<pair<string, int>>& argPairs) {
+	for (auto& arg : argPairs) {
+		writeArg(arg.first, arg.second);
+	}
+}
+
+void sysProcwriteln(vector<pair<string, int>>& argPairs) {
+	sysProcWrite(argPairs);
+	codeSection.append("invoke", "crt_puts,addr zero");
+}
+
+void procStmtRead(const string& arg, int type) {
+	switch (type) {
+	case INTEGER: {
+		codeSection.append("invoke", "crt_scanf,addr intFmt, addr" + arg);
+	}break;
+	case REAL: {
+		codeSection.append("invoke", "crt_scanf,addr realFmt,addr" + arg);
+	}break;
+	case CHAR: {
+		codeSection.append("invoke", "crt_scanf,addr charFmt,addr" + arg);
+	}break;
+	case STRING: {
+		codeSection.append("invoke", "crt_printf,addr stringFmt,addr" + arg);
+	}break;
+	case BOOL: {
+		codeSection.append("invoke", "crt_printf,addr intFmt,addr" + arg);
+	}break;
+	}
+}
+
+void sysFuncSqrt(const string& arg, const string& ret) {
+	codeSection.append("fld", arg);
+	codeSection.append("fsqrt", "");
+	codeSection.append("fstp", ret);
+}
+
+void sysFuncSqr(const string& arg, const string& ret) {
+	codeSection.append("fld", arg);
+	codeSection.append("fmul", arg);
+	codeSection.append("fstp", ret);
+}
+
+void sysFuncAbs(int type,const string& arg, const string& ret) {
+	switch (type) {
+		case INTEGER: {
+			codeSection.append("invoke", "crt_abs," + arg);
+			codeSection.append("mov", ret + ",eax");
+		}break;
+		case REAL: {
+			codeSection.append("fld", arg);
+			codeSection.append("fabs", "");
+			codeSection.append("fstp", ret);
+		}break;
+	}
+
+}
+
+void sysFuncChr(const string& arg, const string& ret) {
+	// int to char
+}
+
+void sysFuncOdd(const string& arg, const string& ret) {
+	// odd = true
+}
+
+void sysFuncOrd(const string& arg, const string& ret) {
+	// char to int
+}
+
+void sysFuncPred(const string & arg, const string & ret)
+{
+	// the last number , char or enum
+}
+
+void sysFuncSucc(const string & arg, const string & ret)
+{
+	// the next number, char or enum
+}
+
+
+void realCalculate(int type, const string& arg1, const string& arg2, const string& ret) {
+	codeSection.append("fld", arg1);
+	string opr;
+	switch (type)
+	{
+		case PLUS:
+		{
+			opr = "fadd";
+		}break;
+		case MINUS:
+		{
+			opr = "fsub";
+		}break;
+		case MUL:
+		{
+			opr = "fmul";
+		}break;
+		case DIV:
+		{
+			opr = "fdiv";
+		}
+		default:
+			break;
+	}
+	codeSection.append(opr,arg2);
+	codeSection.append("fstp",ret);
+}
+
+void realCompare(string arg1, string arg2, string ret) {
+	codeSection.append("fld", arg1);
+	codeSection.append("fcom", arg2);
+	codeSection.append("fstsw", ret);
+	codeSection.append("fwait", "");
+}
+
+
+
+void GenTargetCode(const vector<midcode>&)
+{
+
 }
